e11f58c6f5203d955cf20d1db87e7a0709f4e54b
Guillaume L <guillaume@skillup.co>
2020-11-11 13:43:50 +0100

    [ADD] useToggleRecoilState hook

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as useToggleRecoilState } from './useToggleRecoilState';
---
+++ src/useToggleRecoilState.ts
@@ -0,0 +1,14 @@
+import { useCallback } from 'react';
+import { useRecoilState, RecoilState } from 'recoil';
+
+const useToggleRecoilState = (state: RecoilState<boolean>): [boolean, (nextValue?: any) => void] => {
+  const [value, setValue] = useRecoilState<boolean>(state);
+
+  const toggleValue = useCallback((nextValue?: any): void => {
+    return setValue(typeof nextValue === 'boolean' ? nextValue : !value);
+  }, [value, setValue]);
+
+  return [value, toggleValue];
+};
+
+export default useToggleRecoilState;
