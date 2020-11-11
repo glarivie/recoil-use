005f35fa06c74d50d2a5c7680476c109213390f5
Guillaume L <guillaume@skillup.co>
2020-11-11 15:39:21 +0100

    [ADD] useSetPartialRecoilState hook

---
+++ src/useSetPartialRecoilState.ts
@@ -0,0 +1,16 @@
+import { useCallback } from 'react';
+import { useRecoilState, RecoilState } from 'recoil';
+
+const useSetPartialRecoilState = <T extends object>(
+  state: RecoilState<T>,
+): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
+  const [value, setValue] = useRecoilState<T>(state);
+
+  const setState = useCallback((patch: Partial<T> | ((prevState: T) => Partial<T>)): void => {
+    return setValue(prev => Object.assign({}, prev, patch instanceof Function ? patch(prev) : patch));
+  }, [setValue]);
+
+  return [value, setState];
+};
+
+export default useSetPartialRecoilState;
